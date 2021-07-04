cc932e65bcdccb8c5e134313cb34143f649fd20c
nishant403 <nishantketanshah@gmail.com>
2021-07-04 20:28:33 +0530

    fix : async problem resolved

--- utils/dbStorage.js
+++ utils/dbStorage.js
@@ -10,19 +10,47 @@ function DBProvider(props) {
   const [storage, setStorage] = useState({});
 
   useEffect(() => {
-    db.on("value", function(snapshot) {
+    db.once("value", function(snapshot) {
       snapshot.val() ? setStorage(snapshot.val()) : setStorage({});
       setLoaded(true);
+    });
+
+    db.on("child_added", snapshot => {
+      setStorage(storage => {
+        const key = snapshot.key;
+        const data = snapshot.val();
+
+        let storageCopy = Object.assign({}, storage);
+        storageCopy[key] = data;
+
+        return storageCopy;
+      });
+    });
+
+    db.on("child_changed", snapshot => {
+      setStorage(storage => {
+        const key = snapshot.key;
+        const data = snapshot.val();
+
+        let storageCopy = Object.assign({}, storage);
+        storageCopy[key] = data;
+
+        return storageCopy;
+      });
+    });
+
+    db.on("child_removed", snapshot => {
+      setStorage(storage => {
+        const key = snapshot.key;
+
+        let storageCopy = Object.assign({}, storage);
+        delete storageCopy[key];
+        setStorage(storageCopy);
+      });
     });
   }, []);
 
   async function set(key, data) {
-    if (storage.hasOwnProperty(key) == true) {
-      return remove(key, data).then(() => {
-        return db.child(key).set(data);
-      });
-    }
-
     return db.child(key).set(data);
   }
 
@@ -37,6 +65,8 @@ function DBProvider(props) {
   function get(key) {
     if (storage.hasOwnProperty(key)) {
       return storage[key];
+    } else {
+      console.log("key ", key, "doesn't exist");
     }
   }
 
