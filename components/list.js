e041187dedecf6b7acf4b7fa45ebcb986a8117f5
nishant403 <nishantketanshah@gmail.com>
2021-07-04 13:08:30 +0530

    fix : deep compare in memo using custom-compare

--- components/list.js
+++ components/list.js
@@ -1,21 +1,27 @@
-import React, { useContext, useState, useMemo } from "react";
+import React, { useContext } from "react";
 import { DBContext } from "../utils/dbStorage";
+import { useCustomCompareMemo } from "use-custom-compare";
+import isEqual from "lodash/isEqual";
 
 function List() {
   const context = useContext(DBContext);
+  const counter = context.get("counter");
 
-  const counter = context.get("counter") ? context.get("counter").value : "";
-
-  return useMemo(() => {
-    for (let i = 0; i <= 10000000; i++) {}
-
-    return (
-      <div>
-        <h2>List</h2>
-        <p>{counter}</p>
-      </div>
-    );
-  }, [counter]);
+  return useCustomCompareMemo(
+    () => {
+      for (let i = 0; i <= 10000000; i++) {}
+      return (
+        <div>
+          <h2>List3</h2>
+          <p>{counter ? counter.value : ""}</p>
+        </div>
+      );
+    },
+    [counter],
+    (prevDeps, nextDeps) => {
+      isEqual(prevDeps, nextDeps);
+    }
+  );
 }
 
 export default List;
--- components/list2.js
+++ components/list2.js
@@ -1,20 +1,27 @@
-import React, { useContext, useMemo, useState } from "react";
+import React, { useContext } from "react";
 import { DBContext } from "../utils/dbStorage";
+import { useCustomCompareMemo } from "use-custom-compare";
+import isEqual from "lodash/isEqual";
 
 function List2() {
   const context = useContext(DBContext);
-  const counter = context.get("counter2") ? context.get("counter2").value : "";
+  const counter = context.get("counter2");
 
-  return useMemo(() => {
-    for (let i = 0; i <= 10000000; i++) {}
-
-    return (
-      <div>
-        <h2>List2</h2>
-        <p>{counter}</p>
-      </div>
-    );
-  }, [counter]);
+  return useCustomCompareMemo(
+    () => {
+      for (let i = 0; i <= 10000000; i++) {}
+      return (
+        <div>
+          <h2>List3</h2>
+          <p>{counter ? counter.value : ""}</p>
+        </div>
+      );
+    },
+    [counter],
+    (prevDeps, nextDeps) => {
+      isEqual(prevDeps, nextDeps);
+    }
+  );
 }
 
 export default List2;
